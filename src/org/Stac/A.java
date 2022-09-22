dad6281636fe789f4a4ac05a5133838fea755615
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 16:32:20 +0530

    asaas

--- src/org/Stac/A.java
+++ src/org/Stac/A.java
@@ -1,11 +1,9 @@
 package org.Stac;
 
 public class A {
-	int id=10;
-	public static void main(String[] args) {
-		System.out.println(213321);
-		
-		System.out.println("asxa");
+	private void car() {
+		// TODO Auto-generated method stub
+
 	}
 	}
 

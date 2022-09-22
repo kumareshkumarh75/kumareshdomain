9aa50e8b060b0f7a25ba9fbe6cdc8f9b55ac2c63
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 16:27:24 +0530

    dadaw

--- src/org/Stac/A.java
+++ src/org/Stac/A.java
@@ -5,8 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println(213321);
 		
-		System.out.println("danny");
-		System.out.println();
+		System.out.println("asxa");
 	}
 	}
 
